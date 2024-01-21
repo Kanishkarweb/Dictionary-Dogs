1245f93b9de2e1178b5b896898f9dbadca7ef27f
Kanishkarweb <84016004+Kanishkarweb@users.noreply.github.com>
2024-01-21 12:32:21 +0530

    Add files via upload

---
+++ dogs.py
@@ -0,0 +1,9 @@
+dog = {}
+
+dog = {'name': 'Subramani', 'color': 'brown', 'breed': 'German Shepherd', 'legs': 4, 'age': 5 }
+print(dog)
+print("Name is", dog['name'])
+print("Color is", dog['color'])
+print("Breed is", dog['breed'])
+print("Legs are", dog['legs'])
+print("Age is", dog['age'])
